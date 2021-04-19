d301136fbdae5895e9cd097d409a128498fc3b3a
Janu <janu.m.sung@gmail.com>
2021-04-19 14:44:13 -0700

    building out home page skeleton

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,7 +1,21 @@
 import React from "react";
 
 const Home = (props) => {
-  return <div className="home-cont">I am a home page</div>;
+  const renderVideos = () => {
+    return <div>I'm a video feed</div>;
+  };
+
+  return (
+    <div className="home-cont">
+      <div className="home-search">
+        <div className="home-title">
+          <p>Dance Chives</p>
+          <p>"Dance is the Hidden Language of the Soul"</p>
+        </div>
+      </div>
+      <div className="video-feed">{renderVideos()}</div>
+    </div>
+  );
 };
 
 export default Home;
