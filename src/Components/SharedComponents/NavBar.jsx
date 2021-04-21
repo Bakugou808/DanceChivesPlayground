34df09e061d26835389b9bf8be1cdce81251f814
Janu <janu.m.sung@gmail.com>
2021-04-21 15:59:16 -0700

    working on sub menu for navbar

--- src/Components/SharedComponents/NavBar.jsx
+++ src/Components/SharedComponents/NavBar.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 // * Component Imports
 import SearchBar from "./SearchBar";
 // * Utils Imports
@@ -10,17 +10,102 @@ import {
   // useParams,
   // useRouteMatch,
 } from "react-router-dom";
+import DanceStyles from "../DanceStyles/DanceStyles";
 
 const NavBar = () => {
   let history = useHistory();
+  const [toggleStyles, setStylesToggle] = useState(false);
+  const [toggleEvents, setEventsToggle] = useState(false);
 
   const handleRedirect = (path) => {
     history.push(`${path}`);
   };
 
+  const toggleMenu = (category) => {
+    category === "styles"
+      ? setStylesToggle((prev) => !prev)
+      : setEventsToggle((prev) => !prev);
+  };
+
   return (
     <div className="navBar">
       <p>Logo</p>
+      <div className={toggleStyles ? "dropdown is-active" : "dropdown"}>
+        <div className="dropdown-trigger">
+          <button
+            className="button"
+            aria-haspopup="true"
+            aria-controls="dropdown-menu2"
+            onClick={() => toggleMenu("styles")}
+          >
+            <span>Styles</span>
+            <span className="icon is-small">
+              <i className="fas fa-angle-down" aria-hidden="true"></i>
+            </span>
+          </button>
+        </div>
+        <div className="dropdown-menu" id="dropdown-menu2" role="menu">
+          <div className="dropdown-content">
+            <div className="dropdown-item">
+              <p>Hip-Hop</p>
+            </div>
+            {/* <hr className="dropdown-divider"> */}
+            <div className="dropdown-item">
+              <p>Breaking</p>
+            </div>
+            {/* <hr className="dropdown-divider"> */}
+            <div className="dropdown-item">
+              <p>Popping</p>
+            </div>
+            {/* <hr className="dropdown-divider"> */}
+            <div className="dropdown-item">
+              <p>House</p>
+            </div>
+            {/* <hr className="dropdown-divider"> */}
+            <div className="dropdown-item">
+              <p>Waacking</p>
+            </div>
+            {/* <hr className="dropdown-divider"> */}
+            <div className="dropdown-item">
+              <p>Locking</p>
+            </div>
+            {/* <hr className="dropdown-divider"> */}
+            <div className="dropdown-item">
+              <p>Vogueing</p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className={toggleEvents ? "dropdown is-active" : "dropdown"}>
+        <div className="dropdown-trigger">
+          <button
+            className="button"
+            aria-haspopup="true"
+            aria-controls="dropdown-menu2"
+            onClick={() => toggleMenu("events")}
+          >
+            <span>Events</span>
+            <span className="icon is-small">
+              <i className="fas fa-angle-down" aria-hidden="true"></i>
+            </span>
+          </button>
+        </div>
+        <div className="dropdown-menu" id="dropdown-menu2" role="menu">
+          <div className="dropdown-content">
+            <div className="dropdown-item">
+              <p>Find Events</p>
+            </div>
+            {/* <hr className="dropdown-divider"> */}
+            <div className="dropdown-item">
+              <p>Recent Events</p>
+            </div>
+            {/* <hr className="dropdown-divider"> */}
+            <div className="dropdown-item">
+              <p>Submit Event Info</p>
+            </div>
+          </div>
+        </div>
+      </div>
       <p onClick={() => handleRedirect("home")}>Home</p>
       <p onClick={() => handleRedirect("styles")}>Styles</p>
       <p onClick={() => handleRedirect("events")}>Events</p>
